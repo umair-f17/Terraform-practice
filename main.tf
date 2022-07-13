f9977170de4adb0e4fd58961770da48c287eee25
umair-f17 <umairf17iiui@gmail.com>
2022-07-13 15:43:57 +0500

    networks added

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     google = {
@@ -7,13 +8,42 @@ terraform {
 }
 
 provider "google" {
-    project = "umair-teo"
-    region = "us-central1"
-    zone = "us-central1a"
-  
-}
-resource "google_storage_bucket" "GCSI" {
-  name          = "umair-bucket"
-  force_destroy = true
+  project = "umair-teo"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 
 }
+resource "google_compute_network" "vpc_network" {
+  project                 = "umair-teo"
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges-by-umair" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
