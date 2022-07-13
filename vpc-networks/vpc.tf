ca601ff62f6b069ab27ee7ca88c1e33ae1ee82d5
umair-f17 <umairf17iiui@gmail.com>
2022-07-13 15:47:49 +0500

    netwoork forget

---
+++ vpc-networks/vpc.tf
@@ -0,0 +1,26 @@
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges-by-umair" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
